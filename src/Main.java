65702ea7ad6d7d46e253794b32f477f21fcbee6d
Iampedrojorge <pemjjorge@gmail.com>
2019-05-02 15:57:00 +0100

    testCase e printTree done

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,32 @@ import tree.*;
 import util.DataManager;
 import util.ID3;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         DataManager dataManager = new DataManager();
         ID3 id3 = new ID3();
+
         if(args.length == 0){
             System.out.println("ERROR: No file selected.");
             return;
         }
+
         Tree tree = dataManager.readCSVFile(args[0]);
 
         id3.createTree(tree.getRoot());
         tree.printTree();
 
-        String toTest = "X12,Yes,Yes,Yes,Yes,Full,$,No,No,Burger,30-60";
-        System.out.println();
-        System.out.println(tree.testCase(toTest));
+        if (args.length == 1) {
+            System.out.println("No tests file, exiting.");
+            System.exit(0);
+        }
+        String[] tests = dataManager.readTestFile(args[1]);
+        for (String test : tests) {
+            System.out.println(tree.testCase(test));
+        }
 
     }
 
